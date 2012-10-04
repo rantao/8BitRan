8514549257666a744300928e4c71ea8ffe8d7fa8
Ran Tao <ran@ephexi.com>
2012-10-03 21:34:08 -0700

    Initial Commit

---
+++ 8BitRan/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  8BitRan
+//
+//  Created by Ran Tao on 10.3.12.
+//  Copyright (c) 2012 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
